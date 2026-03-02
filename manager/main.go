6b6ee5c31ab42968a5a48208b5b5e7292b95daf4
Abis <abis@raen.ai>
2026-03-02 20:24:40 +0400

    Main SSH
    - Reading profiles from json
    - Connection SSH
    - Installing SSH lib

--- manager/main.go
+++ manager/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
 	"log"
 	"os"
-	"encoding/json"
+	"time"
+
+	"golang.org/x/crypto/ssh"
 )
 
 type SSHProfile struct {
@@ -23,13 +25,12 @@ func connectionString(profile SSHProfile) string {
 }
 
 func loadProfiles() []SSHProfile {
-	data,err := os.ReadFile("profiles.json")
+	data, err := os.ReadFile("profile.json")
 	if err != nil {
-		log.Fatal("Error reading profiles.json", err)
+		log.Fatal("Error reading profile.json:", err)
 	}
 
 	var profiles []SSHProfile
-	
 	err = json.Unmarshal(data, &profiles)
 	if err != nil {
 		log.Fatal("Error parsing JSON:", err)
@@ -38,11 +39,71 @@ func loadProfiles() []SSHProfile {
 	return profiles
 }
 
+func connectSSH(profile SSHProfile) {
+	config := &ssh.ClientConfig{
+		User: profile.Username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(profile.Password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	address := fmt.Sprintf("%s:%d", profile.Host, profile.Port)
+	fmt.Println("Connecting to", address, "...")
+
+	client, err := ssh.Dial("tcp", address, config)
+	if err != nil {
+		log.Fatal("Failed to connect:", err)
+	}
+	defer client.Close()
+
+	// Start an interactive session
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatal("Failed to create session:", err)
+	}
+	defer session.Close()
+
+	// Connect your terminal to the remote server
+	session.Stdin = os.Stdin
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+
+	// Request a terminal
+	session.RequestPty("xterm", 80, 40, ssh.TerminalModes{})
+
+	// Start shell
+	session.Shell()
+	session.Wait()
+}
+
 func main() {
 	version := "SSH Manager v0.0.1"
 	date_time := time.Now().Format("2006-01-02 15:04:05")
 
 	fmt.Println(version)
 	fmt.Println("Date & Time:", date_time)
+	fmt.Println()
 
+	profiles := loadProfiles()
+
+	for i, profile := range profiles {
+		number := i + 1
+		name := profile.Name
+		connection := connectionString(profile)
+		fmt.Printf("%d. %s - %s\n", number, name, connection)
+	}
+
+	fmt.Println()
+	fmt.Print("Select a profile: ")
+
+	var choice int
+	fmt.Scan(&choice)
+
+	if choice < 1 || choice > len(profiles) {
+		log.Fatal("Invalid choice")
+	}
+
+	selected := profiles[choice-1]
+	connectSSH(selected)
 }
